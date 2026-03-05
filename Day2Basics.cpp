32fbb4f91f830d4d996e57b8c15de7588b5c5b2f
Om codesk <omchaddha7@gmail.com>
2026-03-05 19:44:39 +0530

    Day2Basics.cpp

---
+++ Day2Basics.cpp
@@ -0,0 +1,41 @@
+//if else //
+ // print age if u  are adult or not//
+/* 
+ #include<iostream>
+ using namespace std;
+ int main(){
+    int age;
+    cin>>age;
+     if(age >=18){
+        cout<<"adult";
+
+     }
+     else{
+        cout<<"not adult";
+     }
+     return 0;
+
+ }*/
+//Switch case//
+//take day no and print corresponding day//
+ #include<iostream>
+ using namespace std;
+ int main(){
+     int day;
+     cin>>day;
+
+     switch(day){
+     case 1:
+     cout<<"monday";
+     break;
+     case 2:
+     cout<<"tuesday";
+     break;
+     case 3:
+     cout<<"wednesday";
+     break;
+     default:
+     cout<<"invalid day";
+ }
+ return 0;
+ }
