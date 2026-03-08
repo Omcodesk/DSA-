5e4b42f4d49013bd8ada36fa44aaaacfcf9862b8
Om codesk <omchaddha7@gmail.com>
2026-03-08 22:12:16 +0530

    Day3Functions

--- Day3Function&Complexity.cpp
+++ Day3Function&Complexity.cpp
@@ -6,3 +6,28 @@
 //O(3n) → O(n)
 //Final Time Complexity
 //O(n)
+/*
+void doSomething(int arr[], int n) {
+    arr[0] += 100;
+    cout << "Value inside function: " << arr[0] << endl;
+}
+
+int main() {
+    int n = 5;
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    doSomething(arr, n);
+
+    cout << "Value inside int main: " << arr[0] << endl;
+}*/
+/*
+Step 1 — Program Starts in main()
+int n = 5;
+
+So the program will store 5 numbers.
+
+int arr[n];*/
